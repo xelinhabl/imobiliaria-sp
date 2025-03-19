b852506359232ef848408d39046b4d5b195352a7
xelinha_bl <xelinhabl@gmail.com>
2025-03-19 20:24:52 -0300

    reqsssd

--- backend/imobiliaria/imobiliaria/settings.py
+++ backend/imobiliaria/imobiliaria/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-8eskh@$mrjgi3gx$j&6t25om%g!9%q+k&$m0m-%lja%9+k7m0o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
